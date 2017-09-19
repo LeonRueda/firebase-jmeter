ff6980e29c5237ae81ac7951ac996cf537005d21
Leon Rueda <Phlammariont@gmail.com>
2017-09-19 14:55:13 -0500

    sendMessage for number and interval

--- api/services/MessageService.js
+++ api/services/MessageService.js
@@ -2,14 +2,19 @@
  * Created by pedro.rueda on 19/09/2017.
  */
 
-const Observable = require('rxjs/Observable').Observable;
-require('rxjs/Observable/interval');
-
+const Rx = require('rxjs');
 const Service = {
-  sendMessage: function (number, interval) {
-    let counter = 0;
-    const timer$ = Observable.interval(interval).take(number).subscribe(data => FirebaseService.sendMessage(data));
-
+  send: function (number, interval) {
+    return new Promise((success, error) => {
+      let counter = 0;
+      const timer$ = Rx.Observable.interval(interval)
+        .take(number)
+        .subscribe(
+          data => FirebaseService.sendMessage(data),
+          err => error(err),
+          complete => success(complete)
+        );
+    });
   }
 };
 
